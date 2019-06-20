36f2cebf788e6cac64719e9f34be197f89796430
kz6fittycent <jimmy.tigert@gmail.com>
2019-06-20 16:03:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="bobross",
-    version="1.0.0",
+    version="1.1",
     description="A collection of quotes from Bob Ross",
     long_description=open("README.md").read(),
     license="MIT",
